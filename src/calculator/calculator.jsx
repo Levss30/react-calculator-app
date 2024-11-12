0c87e3b788b60f95fcf48b304e9746aaf3837fd4
Lucas <lucas.levasseur18@gmail.com>
2024-11-12 16:27:21 -0300

    Feat Evaluate

--- src/calculator/calculator.jsx
+++ src/calculator/calculator.jsx
@@ -13,7 +13,15 @@ export const ACTIONS = {
 
 function reducer(state, {type, payload}) {
     switch (type) {
+
         case ACTIONS.ADD_DIGIT:
+            if(state.overwrite){
+                return{
+                    ...state,
+                    currentOperand: payload.digit,
+                    overwrite: false,
+                }
+            }
             if(payload.digit === "0" && state.currentOperand === "0"){
             return state
             }
@@ -24,11 +32,18 @@ function reducer(state, {type, payload}) {
                 ...state,
                 currentOperand: `${state.currentOperand || ""}${payload.digit}`
             }
+
         case ACTIONS.CHOOSE_OPERATION:
-            if(state.currentOperand === null && state.previousOperand === null){
+            if(state.currentOperand == null && state.previousOperand == null){
                 return state
             }
-            if(state.currentOperand === null){
+            if(state.currentOperand == null){
+                return{
+                    ...state,
+                    operation: payload.operation,
+                }
+            }
+            if(state.previousOperand == null){
                 return {
                     ...state,
                     operation: payload.operation,
@@ -37,13 +52,52 @@ function reducer(state, {type, payload}) {
                 }
             }
             return{
-
+                ...state,
+                previousOperand: evaluate(state),
+                operation: payload.operation,
+                currentOperand: null    
             }
 
         case ACTIONS.CLEAR:
             return{}
+
+        case ACTIONS.EVALUATE:
+            if(state.operation == null || state.previousOperand == null || state.currentOperand == null){  
+                return state
+            }
+            return {
+                ...state,
+                previousOperand: null,
+                overwrite: true,
+                operation: null,
+                currentOperand: evaluate(state),
+            }
     }
 
+    function evaluate({currentOperand, previousOperand, operation}){
+        const prev = parseFloat(previousOperand)
+        const current = parseFloat(currentOperand)
+        if(isNaN(prev) || isNaN(current)){
+            return ""
+        }
+        let computation = ""
+        switch(operation){
+            case "+":
+                computation = prev + current
+                break
+            case "-":
+                computation = prev - current
+                break
+            case "÷":
+                computation = prev / current
+                break
+            case "*":
+                computation = prev * current
+                break
+        }
+
+        return computation.toString() 
+    }
 
 }
 
@@ -58,7 +112,7 @@ const Calculator = () => {
         </div>
         <button className='span-two' onClick={() => dispatch({type: ACTIONS.CLEAR })}>AC</button>
         <button>DEL</button>
-        <OperationButton operation="%" dispatch={dispatch} />
+        <OperationButton operation="÷" dispatch={dispatch} />
         <DigitButton digit="1" dispatch={dispatch} />
         <DigitButton digit="2" dispatch={dispatch} />
         <DigitButton digit="3" dispatch={dispatch} />
@@ -73,7 +127,7 @@ const Calculator = () => {
         <OperationButton operation="-" dispatch={dispatch}/>
         <DigitButton digit="." dispatch={dispatch} />
         <DigitButton digit="0" dispatch={dispatch} />
-        <button className='span=two'>=</button>
+        <button className='span=two' onClick={() => dispatch({type: ACTIONS.EVALUATE })}>=</button>
     </div>
   )
 }
