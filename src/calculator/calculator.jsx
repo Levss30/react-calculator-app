8966867e5bf0cc797055dbca0444ff48306fdba9
Lucas <lucas.levasseur18@gmail.com>
2024-11-12 16:12:26 -0300

    Feat CasesACTIONS

--- src/calculator/calculator.jsx
+++ src/calculator/calculator.jsx
@@ -12,13 +12,39 @@ export const ACTIONS = {
 }
 
 function reducer(state, {type, payload}) {
-    switch(type) {
+    switch (type) {
         case ACTIONS.ADD_DIGIT:
+            if(payload.digit === "0" && state.currentOperand === "0"){
+            return state
+            }
+            if(payload.digit === "." && state.currentOperand.includes(".")){
+                return state
+                }    
             return {
                 ...state,
                 currentOperand: `${state.currentOperand || ""}${payload.digit}`
             }
+        case ACTIONS.CHOOSE_OPERATION:
+            if(state.currentOperand === null && state.previousOperand === null){
+                return state
+            }
+            if(state.currentOperand === null){
+                return {
+                    ...state,
+                    operation: payload.operation,
+                    previousOperand: state.currentOperand,
+                    currentOperand: null,
+                }
+            }
+            return{
+
+            }
+
+        case ACTIONS.CLEAR:
+            return{}
     }
+
+
 }
 
 const Calculator = () => {
@@ -30,7 +56,7 @@ const Calculator = () => {
             <div className='previous-operand'>{previousOperand} {operation}</div>
             <div className='current-operand'>{currentOperand}</div>
         </div>
-        <button className='span-two'>AC</button>
+        <button className='span-two' onClick={() => dispatch({type: ACTIONS.CLEAR })}>AC</button>
         <button>DEL</button>
         <OperationButton operation="%" dispatch={dispatch} />
         <DigitButton digit="1" dispatch={dispatch} />
